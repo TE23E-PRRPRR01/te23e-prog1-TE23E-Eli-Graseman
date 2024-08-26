f16958a4956bb1c991e60f5b5dae045efafbcae7
Eli Graseman <>
2024-08-26 14:45:54 +0200

    Vi har tränat lite på if satser

--- Kapitel-1/Registrering/Program.cs
+++ Kapitel-1/Registrering/Program.cs
@@ -1,11 +1,24 @@
 ﻿// Program för att registrera förnamn och efternamn...
+using System.Runtime.CompilerServices;
+
 Console.Clear();
 Console.InputEncoding = System.Text.Encoding.Unicode;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.WriteLine("Program för att registrera förnamn och efternamn");
 Console.WriteLine("Ange förnamn");
 string förnamn = Console.ReadLine();
-Console.WriteLine("Ange efternamn");
+//Console.WriteLine("Ange efternamn");
 string efternamn = Console.ReadLine();
 Console.WriteLine("Hej! " + förnamn + " " + efternamn);
-Console.WriteLine($"Hej! (förnamn) (efternamn)");
+Console.WriteLine($"Hej! {förnamn} {efternamn}");
+
+// Är det Eli som loogar in?
+if (förnamn == "Eli" )
+{
+    Console.WriteLine("Vad trevligt att du är tillbaka");
+}
+
+
+{
+    
+}
