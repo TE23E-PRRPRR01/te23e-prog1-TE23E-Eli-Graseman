6e6279bf8ada149892ef07733bdb40e70f0e9b1b
Eli Graseman <>
2024-08-28 14:43:14 +0200

    Vi har idag fått göra uppgiften "lucktexter" Då har jag övat på att använda Control.Writeline och Console.Readline.

---
+++ Kapitel-1/Lucktext/Program.cs
@@ -0,0 +1,19 @@
+﻿// Lucktext
+
+using System.ComponentModel;
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Välkommen till uppgift 3 lucktext");
+Console.WriteLine("Ange förnamn"); 
+string förnamn = Console.ReadLine();
+Console.WriteLine("Ange efternamn");
+string efternamn = Console.ReadLine(); 
+Console.WriteLine("Ange favorit land");
+string land = Console.ReadLine();
+Console.WriteLine("Ange favorit mat");
+string mat = Console.ReadLine(); 
+Console.WriteLine("Om du har en hobby ange den"); 
+string hobby = Console.ReadLine(); 
+Console.WriteLine($"Hej! {förnamn}, du gillar att resa till {land} det måste vara riktigt skönt att resa dit {förnamn}");
