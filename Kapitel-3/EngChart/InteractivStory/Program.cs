ddb2864b8a3ba62880a06fb8749c3d89ce867859
Eli Graseman <>
2024-09-04 15:04:43 +0200

    nästan klar med uppgiften

--- Kapitel-3/EngChart/InteractivStory/Program.cs
+++ Kapitel-3/EngChart/InteractivStory/Program.cs
@@ -1,25 +1,33 @@
-﻿// Dungeon
+﻿// If satser
 using System.Runtime.CompilerServices;
 
-Console.Clear(); 
-Console.WriteLine("Välkommen till detta spel"); 
+Console.Clear();
+Console.WriteLine("Välkommen till detta spel");
 // Alternativen
-Console.Write("Gillar du musik?(j/n) ");
-string svar = Console.ReadLine();
- // Antingen ja eller nej
- if (svar == "ja") 
- { 
-  Console.WriteLine("Vad roligt! Jag antar att du gillar Hip-Hop?");
- } if (svar == "ja")
- {
-  Console.WriteLine("Fantastiskt det finns inget fel med dig! Har du möjligtvis vädlgit högt IQ?");
-    
- }
- else
- {
-    System.Console.WriteLine();
- }
- else
- {
-    
- }
+Console.Write("Gillar du...?(j/n) ");
+string svar = Console.ReadLine().ToLower();
+// Antingen ja eller nej
+if (svar == "ja")
+{
+   Console.WriteLine("Vad roligt. Jag antar att du är najs....?");
+   if (svar == "ja")
+   {
+      Console.WriteLine("Shit vad najs?");
+      if (svar == "ja")
+      {
+         System.Console.WriteLine("najs");
+      }
+      else
+      {
+         System.Console.WriteLine("onajs");
+      }
+   }
+   else
+   {
+      System.Console.WriteLine("Nähä vad onajs. Jag antar att du är onajs...?");
+   }
+}
+else
+{
+   System.Console.WriteLine("Nähä vad onajs");
+}
