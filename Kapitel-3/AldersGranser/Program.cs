6adc5f2edebb8df5b9b5173d8f49f7cc92f7c3d2
Eli Graseman <>
2024-09-09 14:50:10 +0200

    Tyckte det var svårt idag ska hem och öva, tränat på heltal och konvertering med int.Parse()

---
+++ Kapitel-3/AldersGranser/Program.cs
@@ -0,0 +1,19 @@
+﻿// Vad jag tycker du får göra i olika åldrar
+
+Console.Clear(); 
+Console.WriteLine("Hej! Välkommen till denna frågeställning där jag uttrycker vad jag tycker du får göra beroende på din ålder!");
+
+
+Console.WriteLine("Ange din ålder");
+string dinÅlder = Console.ReadLine();
+int ålder = int.Parse(dinÅlder);
+
+if (ålder == >12) 
+{
+    System.Console.WriteLine("");
+}
+
+if (ålder == 7)
+{
+    
+}
---
+++ Kapitel-3/gissatalet/Program.cs
@@ -0,0 +1,29 @@
+﻿// Ett litet spel - gissa ett hemligt tal
+Console.Clear(); 
+Console.WriteLine("Ett litet spel - gissa ett hemligt tal");
+
+// Slumpa ett tal
+int slumptalet = Random.Shared.Next(1, 101); // 1-100
+ Console.WriteLine($"Slumptalet är {slumptalet}");
+
+Console.Write("Gissa ett tal 1-100)");
+ // Läs in texten, tex "100"
+ string gissningText= Console.ReadLine();
+
+ //Konvertera texten till ett heltal, blir 100
+
+int gissning = int.Parse(gissningText); 
+
+if (gissning == slumptalet)
+{
+    Console.WriteLine("Bra, du har gissat rätt");
+}
+else if (gissning > slumptalet)
+{
+    Console.WriteLine("För högt!");
+}
+else
+{
+    Console.WriteLine("För lågt!");
+}
+
