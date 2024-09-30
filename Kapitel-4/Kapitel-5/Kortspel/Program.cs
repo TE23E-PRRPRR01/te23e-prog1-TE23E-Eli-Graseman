d4d2577826a91c86e2daadf1eafd61e778f14c3b
Eli Graseman <eli.graseman@gmail.com>
2024-09-30 14:52:56 +0200

    Vi har tränat på <> och slump och unicode och emoji

---
+++ Kapitel-4/Kapitel-5/Kortspel/Program.cs
@@ -0,0 +1,33 @@
+﻿// Hur man skapar och använder listor 
+Console.Clear();
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+// Ett annat exempel. lista på 3 årtal
+List<int> årtal = [1957, 1969, 1989];
+Console.WriteLine("Slumpa kort ur en kortlek!");
+
+//Skapa en lista kort 
+//List<string> kortlek = ["🂡", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂬", "🂭", "🂮"];
+List<string> kortlek = ["😃", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂬", "🂭", "🂮"];
+
+
+int antal = 5;
+while (antal > 0)   
+{ 
+    // Slumpa index 0-12
+    int index = Random.Shared.Next(0, kortlek.Count);
+    Console.WriteLine($"Det slumpade index = {index}");
+
+
+    //Plocka ut det sjunde kortet, index = 6
+    string kort = kortlek[index];
+
+    // Ta bort kortet ur kortleken 
+    kortlek.RemoveAt(index);
+
+    //Skriv ut 7:e kortet 
+    Console.WriteLine($"Det slumpade kortet är {kort}");
+
+
+    // Räkna ned
+    antal--;
+}
---
+++ Kapitel-4/Kapitel-5/SlumpaStader/Program.cs
@@ -0,0 +1,17 @@
+﻿// Slumpa städer
+Console.Clear();
+Console.WriteLine("Slumpa Städer!");
+
+
+List<string> städer = ["Karlstad", "Halmstad", "Skellefteå", "Stockholm", "Västerås", "Göteborg", "Nyköping", "Luelå", "Kiruna"];
+
+int antal = 2; 
+while (antal > 0)
+{
+    int index = Random.Shared.Next(0, 10);
+    
+    string stad = städer [index]; 
+
+    Console.WriteLine($"Den slumpade staden är {stad}");
+    antal--; 
+}
