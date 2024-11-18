2a0261609348a463cec69bdfaca053bd8211fde5
Eli Graseman <eli.graseman@gmail.com>
2024-11-18 14:44:00 +0100

    Gjort uppgift i classroom, blev inte klar

---
+++ Kapitel-4/ForLoopTryParse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+// Skapa en loop som körs 10 gånger.
+// Varje gång loopen körs ska den skriva ut ditt namn
+Console.WriteLine("Ange ditt namn");
+string namn = Console.ReadLine();
+while (true)
+{
+    Console.WriteLine("Ange en siffra mellan 1-10");
+    int siffra = int.Parse(Console.ReadLine());
+    
+    for (int i = 1; i <= siffra; i++)
+    {
+        Console.WriteLine($"{i}. {namn}");
+    }
+}
